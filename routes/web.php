1c6392c5786d04585278c2807b3d0dbe9eda0298
MohamedMahmoud96 <mohamed.mahmoud15935@gmail.com>
2022-10-29 13:36:44 +0200

    authorization

--- routes/web.php
+++ routes/web.php
@@ -8,21 +8,3 @@ use MvcPhp\Http\Route;
 
 
 
-Route::get('/', [HomeController::class, 'index']);  
-Route::get('/home', [HomeController::class, 'index']);  
-Route::get('/users', function(){
-    return view('admin/users/users');
-});  
-Route::get('/admin', function(){
-    return view('admin/index');
-});  
-Route::get("/add/new/user", function(){
-    return view('register');
-});
-Route::post('/insertuser', [HomeController::class, 'create']);
-
-Route::get('/logout', [LogoutController::class, 'index']);
-
-Route::get('/login', [LoginController::class, 'index']);
-Route::post('/login', [LoginController::class, 'login']);
-
--- src/Application.php
+++ src/Application.php
@@ -2,6 +2,7 @@
 
 namespace MvcPhp;
 
+use App\Middelware\Authenticate;
 use MvcPhp\Http\Request;
 use MvcPhp\Http\Response;
 use MvcPhp\Http\Route;
@@ -24,6 +25,7 @@ class Application
   public function run()
   {
     session()->start();
+    Authenticate::redirectTo($this->request);
     return $this->route->requestHandell();
   }
 
